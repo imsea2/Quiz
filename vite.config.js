8c911ecfd0b5ebe5cb3181b772f491034437a620
imsea2 <imseaofsea@gmail.com>
2025-08-13 17:16:49 +0900

    plzzzzzzzz

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
 // vite.config.js
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react'   // ← 여기 이름과 설치한 패키지가 반드시 일치!
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Quiz/',   // ← 레포 이름과 정확히 일치, 반드시 슬래시로 감싸기
+  base: '/Quiz/',                          // GitHub Pages 프로젝트 레포면 /레포명/ (앞뒤 슬래시 필수)
 })
